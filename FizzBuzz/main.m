68f3ec11367085ffce54651ba173574dc4556c52
ascoelho <anthonystefancoelho@gmail.com>
2016-04-02 15:49:49 -0400

    FizzBuzz Assignment Completed

--- FizzBuzz/main.m
+++ FizzBuzz/main.m
@@ -11,6 +11,30 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        
+        int i;
+        
+        for (i=1 ; i<101; i++){
+            if (i%3==0 && !(i%5==0)){
+                printf("Fizz\n");
+                
+            }
+            else if (!(i%3==0) && i%5==0) {
+                printf("Buzz\n");
+            }
+            else if (i%3==0 && i%5==0) {
+                printf("FizzBuzz\n");
+            }
+            else{
+               printf("%d\n",i);
+                
+            }
+            
+            
+        }
+        
+        
+        
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
